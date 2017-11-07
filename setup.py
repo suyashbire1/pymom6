803115b8803d8ea4c9248a211730a865e2f4410b
Suyash Bire <suyash309@gmail.com>
2017-11-07 16:43:06 -0500

    Completes dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
-setup(name='pym6',
-      version='0.1',
-      description='Package to analyze mom6 generated data',
-      url='http://github.com/suyashbire1/pym6',
-      author='Suyash Bire',
-      author_email='suyash309@gmail.com',
-      license='MIT',
-      packages=['pym6'],
-      install_requires=[
-          'numpy',
-          'netCDF4',
-      ],
-      zip_safe=False)
+setup(
+    name='pymom6',
+    version='0.1',
+    description='Package to analyze mom6 generated data',
+    url='http://github.com/suyashbire1/pymom6',
+    author='Suyash Bire',
+    author_email='suyash309@gmail.com',
+    license='MIT',
+    packages=['pymom6'],
+    install_requires=[
+        'numba',
+        'netCDF4',
+        'numpy',
+        'xarray',
+    ],
+    zip_safe=False)
